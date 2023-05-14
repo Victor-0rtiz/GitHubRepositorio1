54203082a41e0d6f65f24abba2a7089f277c2cd0
Victor Oss <vicjosneros@gmail.com>
2023-05-13 20:13:33 -0600

    Actualización a una versión más refactorizada v1

--- Program.cs
+++ Program.cs
@@ -1,32 +1,20 @@
-﻿//programa que permita visualizar los departamentos de nicaragua con 
-//su cantidad poblacional.
- //Encuentre: Mayor, menor, sumas y ordene los datos 
-using System.Collections;
+﻿using System.Collections.Generic;
+// Definicion e inicilizacion de los arreglos en paralelo  diccionario
 String[] Departamento = { "Boaco", "Carazo", "Chinandega", "Chontales", "Costa Caribe Norte", "Costa Caribe Sur", "Estelí", "Granada", "Jinotega", "León", "Madriz", "Managua", "Masaya", "Matagalpa", "Nueva Segovia", "Río San Juan", "Rivas" };
 int[] Poblacion = { 185013, 197139, 439906, 190863, 530586, 414543, 229866, 214317, 475630, 421050, 174744, 1546939, 391903, 593503, 271581, 135446, 182645 };
-Dictionary<string, int> diccionario = Departamento
-          .Zip(Poblacion, (k, v) => new { Clave = k, Valor = v })
-          .ToDictionary(x => x.Clave, x => x.Valor);
- //Encontrar el mayor y el menor 
-int maxDep = Poblacion.Max();
-int minDep = Poblacion.Min();
-string maxDepkey = "", minDepkey = "";
-//Encontrar los nombres del mayor y el menor
-for (var i = 0; i < Poblacion.Length; i++)
-{
-    if (Poblacion[i] == maxDep)
-        maxDepkey = Departamento[i];
-    if (Poblacion[i] == minDep)
-        minDepkey = Departamento[i];
-}
-//Ordenando con diccionario y Orderby de LINQ 
-var ordenado = diccionario.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
-Departamento = ordenado.Keys.ToArray();
-Poblacion = ordenado.Values.ToArray();
-//mostrar los resultados
-for (var i = 0; i < Poblacion.Length; i++)
-    Console.WriteLine($"{Departamento[i],20} ==> {Poblacion[i],10:N0}");
-//Sumar todas las poblaciones con LINQ 
+Dictionary<string, int> diccionario = Departamento.Zip(Poblacion, (k, v) => new { Clave = k, Valor = v }).ToDictionary(x => x.Clave, x => x.Valor);
+//Ordenando el diccionario de menor a mayor
+var ordenado = diccionario.OrderBy(x=>x.Value).ToDictionary(x=>x.Key, x=>x.Value);
+//Fijando los nombres de los departamentos con menor y mayor poblacion
+String minDepKey = ordenado.First().Key;
+String maxDepKey = ordenado.Last().Key;
+//reasignacion de los arreglos en paralelo
+Departamento=ordenado.Keys.ToArray();
+Poblacion=ordenado.Values.ToArray();
+//Mostrar los arreglos ordenados de menor a mayor
+for (var i = 0; i < Poblacion.Length; i++){
+    Console.WriteLine($"{Departamento[i],20} ==>  {Poblacion[i],10:N0}");}
+//Suma de toda la poblacion y nombre de mayor y menor
 Console.WriteLine($"Población General:{Poblacion.Sum():N0}");
-Console.WriteLine($"Departamento con mayor Población:{maxDepkey}");
-Console.WriteLine($"Departamento con menor Población:{minDepkey}");
+Console.WriteLine($"Departamento con mayor Población:{maxDepKey}");
+Console.WriteLine($"Departamento con menor Población:{minDepKey}");
