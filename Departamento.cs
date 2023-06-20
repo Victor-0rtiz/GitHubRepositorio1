b766bfc03ffbb58438869b5d2f9c7e3cdc247c21
Victor Oss <vicjosneros@gmail.com>
2023-06-19 22:11:25 -0600

    quitando archivos innecesarios con gitignore

---
+++ Departamento.cs
@@ -0,0 +1,13 @@
+namespace DepartamentosClass
+{
+    public class Dpto
+    {
+        public string Name {get; set;}
+        public int population {get; set;}
+        public Dpto ( string name , int poblacion){
+            Name = name;
+            population = poblacion;
+        }
+        
+    }
+}
